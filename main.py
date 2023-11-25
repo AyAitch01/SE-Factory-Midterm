77ed00b691143beaaf9ae9331bff1f589c262db6
abdul <abdulrahmanhabli01@gmail.com>
2023-11-25 19:51:36 +0200

    creating greeting message and the user menu

---
+++ main.py
@@ -0,0 +1,13 @@
+def printlist():
+    print("***Welcome***")
+    print('''1. Open Tab
+2. Close Tab
+3. Switch Tab
+4. Display All Tabs
+5. Open Nested Tab
+6. Sort All Tabs
+7. Save Tabs
+8. Import Tabs
+9. Exit''')
+
+printlist()
